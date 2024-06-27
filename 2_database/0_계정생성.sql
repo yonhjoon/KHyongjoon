832cda69fe8d5a075a217fceab83e3c2f67a184a
yongjoon <missjj312@nver.com>
2024-06-28 00:54:15 +0900

    no message

--- 2_database/0_계정생성.sql
+++ 2_database/0_계정생성.sql
@@ -11,9 +11,9 @@ SELECT * FROM DBA_USERS; -- 현재 모든 계정들에 대해서 조회하는 명령문
 
 --일반 사용자 계정을 생성하는 구문 (오직 관리자 계정에서만 할 수 있다.)
 --[표현식] CREATE USER 계정명 IDENTIFIED BY 비밀번호;
-CREATE USER KH IDENTIFIED BY KH;
+CREATE USER SEMI IDENTIFIED BY SEMI;
 --계정의 비밀번호는 대소문자를 구분한다.
 
 --위에서 생성된 일반 사용자계정에 최소한의 권한 (접속, 데이터관리) 부여
 -- [표현법] GRANT 권한1, 권한2... TO 계정명;
-GRANT RESOURCE, CONNECT TO KH;
+GRANT RESOURCE, CONNECT TO SEMI;
